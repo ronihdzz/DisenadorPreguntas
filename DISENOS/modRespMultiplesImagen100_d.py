cd8d3ff8deb2a4e9b710ec3e6bc008f12479c2df
ronihdz <ronaldo.runing_@hotmail.com>
2020-08-04 13:57:25 -0500

    avance de las preguntas multiples

---
+++ DISENOS/modRespMultiplesImagen100_d.py
@@ -0,0 +1,269 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'modRespMultiplesImagen100_d.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(848, 376)
+        self.btn_Datos = QtWidgets.QPushButton(Form)
+        self.btn_Datos.setGeometry(QtCore.QRect(270, 70, 81, 71))
+        font = QtGui.QFont()
+        font.setPointSize(60)
+        self.btn_Datos.setFont(font)
+        self.btn_Datos.setStyleSheet("QPushButton {\n"
+"/*color: #333;*/\n"
+"border: 1px solid #555;\n"
+"border-radius: 20px;\n"
+"border-style: outset;\n"
+"/*background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
+");*/\n"
+"padding: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
+");\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"border-style: inset;\n"
+"background: qradialgradient(\n"
+"cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+");\n"
+"}")
+        self.btn_Datos.setObjectName("btn_Datos")
+        self.txtEdit_preg = QtWidgets.QTextEdit(Form)
+        self.txtEdit_preg.setGeometry(QtCore.QRect(10, 10, 251, 211))
+        self.txtEdit_preg.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_preg.setObjectName("txtEdit_preg")
+        self.btn_Datos_2 = QtWidgets.QPushButton(Form)
+        self.btn_Datos_2.setGeometry(QtCore.QRect(560, 70, 81, 71))
+        font = QtGui.QFont()
+        font.setPointSize(60)
+        self.btn_Datos_2.setFont(font)
+        self.btn_Datos_2.setStyleSheet("QPushButton {\n"
+"/*color: #333;*/\n"
+"border: 1px solid #555;\n"
+"border-radius: 20px;\n"
+"border-style: outset;\n"
+"/*background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
+");*/\n"
+"padding: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
+");\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"border-style: inset;\n"
+"background: qradialgradient(\n"
+"cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+");\n"
+"}")
+        self.btn_Datos_2.setObjectName("btn_Datos_2")
+        self.txt_b = QtWidgets.QTextEdit(Form)
+        self.txt_b.setGeometry(QtCore.QRect(10, 280, 251, 81))
+        self.txt_b.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txt_b.setObjectName("txt_b")
+        self.txtEdit_a = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a.setGeometry(QtCore.QRect(360, 30, 191, 151))
+        self.txtEdit_a.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a.setObjectName("txtEdit_a")
+        self.txtEdit_a_5 = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a_5.setGeometry(QtCore.QRect(650, 30, 191, 151))
+        self.txtEdit_a_5.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a_5.setObjectName("txtEdit_a_5")
+        self.btn_Datos_3 = QtWidgets.QPushButton(Form)
+        self.btn_Datos_3.setGeometry(QtCore.QRect(560, 250, 81, 71))
+        font = QtGui.QFont()
+        font.setPointSize(60)
+        self.btn_Datos_3.setFont(font)
+        self.btn_Datos_3.setStyleSheet("QPushButton {\n"
+"/*color: #333;*/\n"
+"border: 1px solid #555;\n"
+"border-radius: 20px;\n"
+"border-style: outset;\n"
+"/*background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
+");*/\n"
+"padding: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
+");\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"border-style: inset;\n"
+"background: qradialgradient(\n"
+"cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+");\n"
+"}")
+        self.btn_Datos_3.setObjectName("btn_Datos_3")
+        self.txtEdit_a_6 = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a_6.setGeometry(QtCore.QRect(650, 210, 191, 151))
+        self.txtEdit_a_6.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a_6.setObjectName("txtEdit_a_6")
+        self.txtEdit_a_2 = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a_2.setGeometry(QtCore.QRect(360, 210, 191, 151))
+        self.txtEdit_a_2.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a_2.setObjectName("txtEdit_a_2")
+        self.btn_Datos_4 = QtWidgets.QPushButton(Form)
+        self.btn_Datos_4.setGeometry(QtCore.QRect(270, 250, 81, 71))
+        font = QtGui.QFont()
+        font.setPointSize(60)
+        self.btn_Datos_4.setFont(font)
+        self.btn_Datos_4.setStyleSheet("QPushButton {\n"
+"/*color: #333;*/\n"
+"border: 1px solid #555;\n"
+"border-radius: 20px;\n"
+"border-style: outset;\n"
+"/*background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
+");*/\n"
+"padding: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
+");\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"border-style: inset;\n"
+"background: qradialgradient(\n"
+"cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+");\n"
+"}")
+        self.btn_Datos_4.setObjectName("btn_Datos_4")
+        self.label_7 = QtWidgets.QLabel(Form)
+        self.label_7.setGeometry(QtCore.QRect(300, 20, 51, 41))
+        self.label_7.setStyleSheet("border-image: url(:/alinear/IMAGENES/imagen_subir.png);\n"
+"")
+        self.label_7.setText("")
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(Form)
+        self.label_8.setGeometry(QtCore.QRect(590, 20, 51, 41))
+        self.label_8.setStyleSheet("border-image: url(:/alinear/IMAGENES/imagen_subir.png);\n"
+"")
+        self.label_8.setText("")
+        self.label_8.setObjectName("label_8")
+        self.label_9 = QtWidgets.QLabel(Form)
+        self.label_9.setGeometry(QtCore.QRect(300, 200, 51, 41))
+        self.label_9.setStyleSheet("border-image: url(:/alinear/IMAGENES/imagen_subir.png);\n"
+"")
+        self.label_9.setText("")
+        self.label_9.setObjectName("label_9")
+        self.label_10 = QtWidgets.QLabel(Form)
+        self.label_10.setGeometry(QtCore.QRect(590, 200, 51, 41))
+        self.label_10.setStyleSheet("border-image: url(:/alinear/IMAGENES/imagen_subir.png);\n"
+"")
+        self.label_10.setText("")
+        self.label_10.setObjectName("label_10")
+        self.doubleSpinBox_2 = QtWidgets.QDoubleSpinBox(Form)
+        self.doubleSpinBox_2.setGeometry(QtCore.QRect(170, 240, 69, 26))
+        self.doubleSpinBox_2.setObjectName("doubleSpinBox_2")
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(140, 240, 21, 21))
+        self.label_5.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_derecho.png);")
+        self.label_5.setText("")
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Form)
+        self.label_6.setGeometry(QtCore.QRect(80, 240, 21, 21))
+        self.label_6.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_centrar.png);")
+        self.label_6.setText("")
+        self.label_6.setObjectName("label_6")
+        self.label_4 = QtWidgets.QLabel(Form)
+        self.label_4.setGeometry(QtCore.QRect(110, 240, 21, 21))
+        self.label_4.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_izquierda.png);")
+        self.label_4.setText("")
+        self.label_4.setObjectName("label_4")
+        self.label_11 = QtWidgets.QLabel(Form)
+        self.label_11.setGeometry(QtCore.QRect(20, 230, 51, 41))
+        self.label_11.setStyleSheet("border-image: url(:/alinear/IMAGENES/imagen_subir.png);\n"
+"")
+        self.label_11.setText("")
+        self.label_11.setObjectName("label_11")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.btn_Datos.setText(_translate("Form", "A"))
+        self.btn_Datos_2.setText(_translate("Form", "B"))
+        self.btn_Datos_3.setText(_translate("Form", "D"))
+        self.btn_Datos_4.setText(_translate("Form", "C"))
+
+import imagenes_rc
--- DISENOS/modRespMultiplesImagen50_d.py
+++ DISENOS/modRespMultiplesImagen50_d.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'modRespMultiples_d.ui'
+# Form implementation generated from reading ui file 'modRespMultiplesImagen50_d.ui'
 #
 # Created by: PyQt5 UI code generator 5.10.1
 #
@@ -11,20 +11,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(852, 376)
-        self.txt_b = QtWidgets.QTextEdit(Form)
-        self.txt_b.setGeometry(QtCore.QRect(150, 280, 251, 81))
-        self.txt_b.setStyleSheet("  background-color: #19232D;\n"
-"  border: 0px solid #32414B;\n"
-"  padding: 0px;\n"
-"  color: #F0F0F0;\n"
-"  selection-background-color: #1464A0;\n"
-"  selection-color: #F0F0F0;\n"
-"\n"
-"    border-radius: 10;")
-        self.txt_b.setObjectName("txt_b")
+        Form.resize(850, 377)
         self.btn_Datos_4 = QtWidgets.QPushButton(Form)
-        self.btn_Datos_4.setGeometry(QtCore.QRect(430, 280, 89, 81))
+        self.btn_Datos_4.setGeometry(QtCore.QRect(310, 300, 81, 71))
         font = QtGui.QFont()
         font.setPointSize(60)
         self.btn_Datos_4.setFont(font)
@@ -55,41 +44,8 @@ class Ui_Form(object):
 ");\n"
 "}")
         self.btn_Datos_4.setObjectName("btn_Datos_4")
-        self.txt_d = QtWidgets.QTextEdit(Form)
-        self.txt_d.setGeometry(QtCore.QRect(530, 280, 251, 81))
-        self.txt_d.setStyleSheet("  background-color: #19232D;\n"
-"  border: 0px solid #32414B;\n"
-"  padding: 0px;\n"
-"  color: #F0F0F0;\n"
-"  selection-background-color: #1464A0;\n"
-"  selection-color: #F0F0F0;\n"
-"\n"
-"    border-radius: 10;")
-        self.txt_d.setObjectName("txt_d")
-        self.txtEdit_a = QtWidgets.QTextEdit(Form)
-        self.txtEdit_a.setGeometry(QtCore.QRect(150, 190, 251, 81))
-        self.txtEdit_a.setStyleSheet("  background-color: #19232D;\n"
-"  border: 0px solid #32414B;\n"
-"  padding: 0px;\n"
-"  color: #F0F0F0;\n"
-"  selection-background-color: #1464A0;\n"
-"  selection-color: #F0F0F0;\n"
-"\n"
-"    border-radius: 10;")
-        self.txtEdit_a.setObjectName("txtEdit_a")
-        self.txt_c = QtWidgets.QTextEdit(Form)
-        self.txt_c.setGeometry(QtCore.QRect(530, 190, 251, 81))
-        self.txt_c.setStyleSheet("  background-color: #19232D;\n"
-"  border: 0px solid #32414B;\n"
-"  padding: 0px;\n"
-"  color: #F0F0F0;\n"
-"  selection-background-color: #1464A0;\n"
-"  selection-color: #F0F0F0;\n"
-"\n"
-"    border-radius: 10;")
-        self.txt_c.setObjectName("txt_c")
         self.btn_Datos = QtWidgets.QPushButton(Form)
-        self.btn_Datos.setGeometry(QtCore.QRect(50, 190, 89, 81))
+        self.btn_Datos.setGeometry(QtCore.QRect(310, 60, 81, 71))
         font = QtGui.QFont()
         font.setPointSize(60)
         self.btn_Datos.setFont(font)
@@ -120,40 +76,8 @@ class Ui_Form(object):
 ");\n"
 "}")
         self.btn_Datos.setObjectName("btn_Datos")
-        self.btn_Datos_3 = QtWidgets.QPushButton(Form)
-        self.btn_Datos_3.setGeometry(QtCore.QRect(430, 190, 89, 81))
-        font = QtGui.QFont()
-        font.setPointSize(60)
-        self.btn_Datos_3.setFont(font)
-        self.btn_Datos_3.setStyleSheet("QPushButton {\n"
-"/*color: #333;*/\n"
-"border: 1px solid #555;\n"
-"border-radius: 20px;\n"
-"border-style: outset;\n"
-"/*background: qradialgradient(\n"
-"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
-"radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
-");*/\n"
-"padding: 5px;\n"
-"}\n"
-"\n"
-"QPushButton:hover {\n"
-"background: qradialgradient(\n"
-"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
-"radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
-");\n"
-"}\n"
-"\n"
-"QPushButton:pressed {\n"
-"border-style: inset;\n"
-"background: qradialgradient(\n"
-"cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
-"radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
-");\n"
-"}")
-        self.btn_Datos_3.setObjectName("btn_Datos_3")
         self.btn_Datos_2 = QtWidgets.QPushButton(Form)
-        self.btn_Datos_2.setGeometry(QtCore.QRect(50, 280, 89, 81))
+        self.btn_Datos_2.setGeometry(QtCore.QRect(310, 140, 81, 71))
         font = QtGui.QFont()
         font.setPointSize(60)
         self.btn_Datos_2.setFont(font)
@@ -184,8 +108,40 @@ class Ui_Form(object):
 ");\n"
 "}")
         self.btn_Datos_2.setObjectName("btn_Datos_2")
+        self.btn_Datos_3 = QtWidgets.QPushButton(Form)
+        self.btn_Datos_3.setGeometry(QtCore.QRect(310, 220, 81, 71))
+        font = QtGui.QFont()
+        font.setPointSize(60)
+        self.btn_Datos_3.setFont(font)
+        self.btn_Datos_3.setStyleSheet("QPushButton {\n"
+"/*color: #333;*/\n"
+"border: 1px solid #555;\n"
+"border-radius: 20px;\n"
+"border-style: outset;\n"
+"/*background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #888\n"
+");*/\n"
+"padding: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"background: qradialgradient(\n"
+"cx: 0.3, cy: -0.4, fx: 0.3, fy: -0.4,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #bbb\n"
+");\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"border-style: inset;\n"
+"background: qradialgradient(\n"
+"cx: 0.4, cy: -0.1, fx: 0.4, fy: -0.1,\n"
+"radius: 1.35, stop: 0 #fff, stop: 1 #ddd\n"
+");\n"
+"}")
+        self.btn_Datos_3.setObjectName("btn_Datos_3")
         self.txtEdit_preg = QtWidgets.QTextEdit(Form)
-        self.txtEdit_preg.setGeometry(QtCore.QRect(150, 40, 631, 111))
+        self.txtEdit_preg.setGeometry(QtCore.QRect(30, 160, 251, 211))
         self.txtEdit_preg.setStyleSheet("  background-color: #19232D;\n"
 "  border: 0px solid #32414B;\n"
 "  padding: 0px;\n"
@@ -195,42 +151,103 @@ class Ui_Form(object):
 "\n"
 "    border-radius: 10;")
         self.txtEdit_preg.setObjectName("txtEdit_preg")
+        self.txtEdit_a = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a.setGeometry(QtCore.QRect(400, 60, 431, 71))
+        self.txtEdit_a.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a.setObjectName("txtEdit_a")
+        self.txtEdit_a_2 = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a_2.setGeometry(QtCore.QRect(400, 140, 431, 71))
+        self.txtEdit_a_2.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a_2.setObjectName("txtEdit_a_2")
+        self.txtEdit_a_3 = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a_3.setGeometry(QtCore.QRect(400, 220, 431, 71))
+        self.txtEdit_a_3.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a_3.setObjectName("txtEdit_a_3")
+        self.txtEdit_a_4 = QtWidgets.QTextEdit(Form)
+        self.txtEdit_a_4.setGeometry(QtCore.QRect(400, 300, 431, 71))
+        self.txtEdit_a_4.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txtEdit_a_4.setObjectName("txtEdit_a_4")
+        self.doubleSpinBox = QtWidgets.QDoubleSpinBox(Form)
+        self.doubleSpinBox.setGeometry(QtCore.QRect(760, 20, 69, 26))
+        self.doubleSpinBox.setObjectName("doubleSpinBox")
         self.doubleSpinBox_2 = QtWidgets.QDoubleSpinBox(Form)
-        self.doubleSpinBox_2.setGeometry(QtCore.QRect(710, 160, 69, 26))
+        self.doubleSpinBox_2.setGeometry(QtCore.QRect(210, 20, 69, 26))
         self.doubleSpinBox_2.setObjectName("doubleSpinBox_2")
-        self.label_5 = QtWidgets.QLabel(Form)
-        self.label_5.setGeometry(QtCore.QRect(680, 160, 21, 21))
-        self.label_5.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_derecho.png);")
-        self.label_5.setText("")
-        self.label_5.setObjectName("label_5")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(670, 20, 21, 21))
+        self.label.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_centrar.png);")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(730, 20, 21, 21))
+        self.label_2.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_derecho.png);")
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Form)
+        self.label_3.setGeometry(QtCore.QRect(700, 20, 21, 21))
+        self.label_3.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_izquierda.png);")
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
         self.label_4 = QtWidgets.QLabel(Form)
-        self.label_4.setGeometry(QtCore.QRect(650, 160, 21, 21))
+        self.label_4.setGeometry(QtCore.QRect(150, 20, 21, 21))
         self.label_4.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_izquierda.png);")
         self.label_4.setText("")
         self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(Form)
+        self.label_5.setGeometry(QtCore.QRect(180, 20, 21, 21))
+        self.label_5.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_derecho.png);")
+        self.label_5.setText("")
+        self.label_5.setObjectName("label_5")
         self.label_6 = QtWidgets.QLabel(Form)
-        self.label_6.setGeometry(QtCore.QRect(620, 160, 21, 21))
+        self.label_6.setGeometry(QtCore.QRect(120, 20, 21, 21))
         self.label_6.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_centrar.png);")
         self.label_6.setText("")
         self.label_6.setObjectName("label_6")
+        self.txt_b = QtWidgets.QTextEdit(Form)
+        self.txt_b.setGeometry(QtCore.QRect(30, 60, 251, 91))
+        self.txt_b.setStyleSheet("  background-color: #19232D;\n"
+"  border: 0px solid #32414B;\n"
+"  padding: 0px;\n"
+"  color: #F0F0F0;\n"
+"  selection-background-color: #1464A0;\n"
+"  selection-color: #F0F0F0;\n"
+"\n"
+"    border-radius: 10;")
+        self.txt_b.setObjectName("txt_b")
         self.label_7 = QtWidgets.QLabel(Form)
-        self.label_7.setGeometry(QtCore.QRect(680, 10, 21, 21))
-        self.label_7.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_derecho.png);")
+        self.label_7.setGeometry(QtCore.QRect(60, 0, 51, 51))
+        self.label_7.setStyleSheet("border-image: url(:/alinear/IMAGENES/imagen_subir.png);\n"
+"border-image: url(:/alinear/IMAGENES/imagen_carga.png);")
         self.label_7.setText("")
         self.label_7.setObjectName("label_7")
-        self.label_8 = QtWidgets.QLabel(Form)
-        self.label_8.setGeometry(QtCore.QRect(650, 10, 21, 21))
-        self.label_8.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_izquierda.png);")
-        self.label_8.setText("")
-        self.label_8.setObjectName("label_8")
-        self.label_9 = QtWidgets.QLabel(Form)
-        self.label_9.setGeometry(QtCore.QRect(620, 10, 21, 21))
-        self.label_9.setStyleSheet("border-image: url(:/alinear/IMAGENES/alinear_centrar.png);")
-        self.label_9.setText("")
-        self.label_9.setObjectName("label_9")
-        self.doubleSpinBox_3 = QtWidgets.QDoubleSpinBox(Form)
-        self.doubleSpinBox_3.setGeometry(QtCore.QRect(710, 10, 69, 26))
-        self.doubleSpinBox_3.setObjectName("doubleSpinBox_3")
 
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
@@ -240,7 +257,7 @@ class Ui_Form(object):
         Form.setWindowTitle(_translate("Form", "Form"))
         self.btn_Datos_4.setText(_translate("Form", "D"))
         self.btn_Datos.setText(_translate("Form", "A"))
-        self.btn_Datos_3.setText(_translate("Form", "C"))
         self.btn_Datos_2.setText(_translate("Form", "B"))
+        self.btn_Datos_3.setText(_translate("Form", "C"))
 
 import imagenes_rc
